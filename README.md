da181c89047fed4a27572ccee244187d82a5aff2
Raghvendra Singh <raghvendrapratap.success@gmail.com>
2021-07-25 21:15:10 +0530

    Completed webpack course

---
+++ README.md
@@ -0,0 +1,3 @@
+### Learning Webpack
+
+This repo is created by following tutorial only for learning and practicing webpack :v: :wink:.
---
+++ webpack.common.js
@@ -0,0 +1,14 @@
+const path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: { main: "./src/index.js", vendor: "./src/vendor.js" },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+    ],
+  },
+};
